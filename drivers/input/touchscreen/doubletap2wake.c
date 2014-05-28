3e2a3ee71b59f1d4d137dd0a69fa0ba8b0e5d72c
renderbroken <zkennedy87@gmail.com>
2014-05-28 08:26:23 -0700

    s2w/dt2w: implement Pocket mode and make the features usable in deep sleep THX to Alin23!!!
    Signed-off-by: renderbroken <zkennedy87@gmail.com>
    
    Conflicts:
    	drivers/input/touchscreen/sweep2wake.c
    
    Conflicts:
    	drivers/input/touchscreen/sweep2wake.c

--- drivers/input/touchscreen/doubletap2wake.c
+++ drivers/input/touchscreen/doubletap2wake.c
@@ -58,7 +58,7 @@ MODULE_LICENSE("GPLv2");
 #define DT2W_DEBUG		0
 #define DT2W_DEFAULT		0
 
-#define DT2W_PWRKEY_DUR		60
+#define DT2W_PWRKEY_DUR		10
 #define DT2W_FEATHER		200
 #define DT2W_TIME		700
 
