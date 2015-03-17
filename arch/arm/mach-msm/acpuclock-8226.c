ac38ba8ed02356f51a234bc95344f52ea1dec48c
desaishivam26 <kane.desai@gmail.com>
2015-03-17 16:06:15 +0530

    Fix UC

--- arch/arm/mach-msm/acpuclock-8226.c
+++ arch/arm/mach-msm/acpuclock-8226.c
@@ -169,8 +169,8 @@ static struct acpuclk_drv_data drv_data = {
 		.update_mask = RCG_CONFIG_UPDATE_BIT,
 		.poll_mask = RCG_CONFIG_UPDATE_BIT,
 	},
-	.power_collapse_khz = 300000,
-	.wait_for_irq_khz = 300000,
+	.power_collapse_khz = 192000,
+	.wait_for_irq_khz = 192000,
 };
 
 static int __init acpuclk_a7_probe(struct platform_device *pdev)
